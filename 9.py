fd104228aef4d42eee39528a6d45ddd9283360a0
tobasi-21 <tobasitest@gmail.com>
2021-07-23 02:41:45 +0800

    wdwdw

--- 9.py
+++ 9.py
@@ -1,9 +1,14 @@
 import pytest
 import os 
+import allure
+
+
+
 
 def test_aa():
     a=1
     b=2
-    assert a==b
-    os.system("exit 1")
+
+    # os.system("exit 1")
+    print('222222222222222')
 # print('ss')
